94bf804c5ce078cb6464278e294ecc80951b96ec
Matteo Pacini <ispeakprogramming@gmail.com>
2013-07-25 23:58:17 +0200

    Updated license. Some documentation written

--- Soundboard/Soundboard.h
+++ Soundboard/Soundboard.h
@@ -1,17 +1,21 @@
-//This file is part of Soundboard.
-//
-//Soundboard is free software: you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
-
-//Soundboard is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-
-//You should have received a copy of the GNU General Public License
-//along with Soundboard.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////
+//                                                                        //
+//  This file is part of Soundboard.                                      //
+//                                                                        //
+//  Soundboard is free software: you can redistribute it and/or modify    //
+//  it under the terms of the GNU General Public License as published by  //
+//  the Free Software Foundation, either version 3 of the License, or     //
+//  any later version.                                                    //
+//                                                                        //
+//  Soundboard is distributed in the hope that it will be useful,         //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+//  GNU General Public License for more details.                          //
+//                                                                        //
+//  You should have received a copy of the GNU General Public License     //
+//  along with Soundboard.  If not, see <http://www.gnu.org/licenses/>.   //
+//                                                                        //
+////////////////////////////////////////////////////////////////////////////
 
 #import <Foundation/Foundation.h>
 
@@ -43,10 +47,19 @@ Soundboard date.
 
 #pragma mark - Constructor
 
+/**
+ Initializes a new Soundboard object for the given soundboard at path <B>path</B>.
+ @param path A NSString object represeting the path of the soundboard
+ @returns a newly initialized Soundboard object
+ */
 -(id)initWithFile:(NSString*)path;
 
 #pragma mark - Icon methods
 
+/**
+ Returns the icon data.
+ @returns a NSData object.
+ */
 -(NSData*)iconData;
 
 #pragma mark - Clip methods
